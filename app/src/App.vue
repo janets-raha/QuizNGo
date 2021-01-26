24ea738204a65a20608ebc487c627e372056dfda
janets-raha <jeannette.cecora@gmail.com>
2021-01-26 22:39:13 +0100

    add QuizNGo

---
+++ app/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="bg-light">
+    <NavBar
+      :isLoggedIn="this.$store.getters.loggedIn"
+      :isAdmin="this.$store.getters.isAdmin"
+    />
+    <router-view />
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  components: {
+    NavBar,
+  },
+};
+</script>
